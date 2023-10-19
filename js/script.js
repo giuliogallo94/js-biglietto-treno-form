8dc7e2066f76cb317c807229064895aabbb8dabc
giuliogallo94 <giuliogallo1994@gmail.com>
2023-10-19 17:55:57 +0200

    add: js logic

--- js/script.js
+++ js/script.js
@@ -1,13 +1,47 @@
+const nameUserElem = document.getElementById("name-user");
+
 const kmUserElem = document.getElementById("km-user");
 
 const ageUserElem = document.getElementById("age-user");
 
-const userPrice = document.querySelector(".user-price");
+let userPrice = document.querySelector(".user-price");
+
+// DATI
 
 const sendBtn = document.getElementById("send");
+
 sendBtn.addEventListener("click", function () {
   console.log("Funziona!");
+  const nameUser = nameUserElem.value;
   const kmUser = parseInt(kmUserElem.value);
-  const ageUser = parseInt(ageUserElem.value);
-  console.log(kmUser, ageUser);
+  const ageUser = ageUserElem.value;
+  console.log(nameUser, kmUser, ageUser);
+
+  const kmPrice = 0.21;
+
+  // LOGICA
+
+  const standardPrice = kmUser * kmPrice;
+  console.log(standardPrice);
+
+  const underPrice = standardPrice - standardPrice * 0.2;
+  const overPrice = standardPrice - standardPrice * 0.4;
+
+  let userPrice = "";
+
+  if (ageUser === "underAge") {
+    userPrice = underPrice;
+  } else if (ageUser === "overAge") {
+    userPrice = overPrice;
+  } else {
+    userPrice = standardPrice;
+  }
+  console.log(userPrice);
 });
+
+// OUTPUT
+function getOption() {
+  selectElement = document.querySelector("#select1");
+  output = selectElement.options[selectElement.selectedIndex].value;
+  document.querySelector(".output").textContent = output;
+}
